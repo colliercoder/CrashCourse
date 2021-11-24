b2c4981606f4ef8c6a06e2e48038fc274afd3ef6
Dan Gauerke <89668769+colliercoder@users.noreply.github.com>
2021-11-23 21:02:04 -0500

    commit

---
+++ 2cars.py
@@ -0,0 +1,7 @@
+from car import Car, ElectricCar
+
+my_beetle = Car('volvswagen','beetle', 2018)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = ElectricCar('tesla','model s',2021)
+print(my_tesla.get_descriptive_name())
